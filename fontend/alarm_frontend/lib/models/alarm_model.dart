733c7a220979408d85b3b2f23e03afb06d783e3a
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-03-21 13:00:38 +0530

    alarm model file created

---
+++ fontend/alarm_frontend/lib/models/alarm_model.dart
@@ -0,0 +1,11 @@
+class AlarmModel {
+  final String title;
+  final String time;
+  final String date;
+
+  const AlarmModel({
+    required this.title,
+    required this.time,
+    required this.date,
+  });
+}
