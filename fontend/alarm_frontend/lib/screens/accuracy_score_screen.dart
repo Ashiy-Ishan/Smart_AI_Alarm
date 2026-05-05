92210b812c4c9901ed7cbd2ad83c6f4aec9108e3
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-05-06 01:45:41 +0530

    beside hub sub pages created

---
+++ fontend/alarm_frontend/lib/screens/accuracy_score_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccuracyScoreScreen extends StatefulWidget {
+  const AccuracyScoreScreen({super.key});
+
+  @override
+  State<AccuracyScoreScreen> createState() => _AccuracyScoreScreenState();
+}
+
+class _AccuracyScoreScreenState extends State<AccuracyScoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
